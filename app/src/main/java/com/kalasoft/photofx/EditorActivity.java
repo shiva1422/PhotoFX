c8016c25543e7e712012cdc57aac755ebbd5f679
KalaSoft1422 <pspr142@gmail.com>
2021-04-16 11:23:51 -0500

    toWindows

--- app/src/main/java/com/kalasoft/photofx/EditorActivity.java
+++ app/src/main/java/com/kalasoft/photofx/EditorActivity.java
@@ -81,7 +81,7 @@ public class EditorActivity extends NativeActivity implements ActivityCompat.OnR
         Log.e("getBitmap", "the id is " + id);
         switch (id) {
             case 0:
-                bitmapId = R.drawable.image;
+                bitmapId = R.drawable.fourk;
                 break;
             case 1:
                 // bitmapId=R.drawable.ssnare;
